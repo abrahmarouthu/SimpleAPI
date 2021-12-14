dac5de103e638cb0457d6a4128d0a32af889d931
Ashwin Brahmarouthu <ashwin.brahmarouthu@dignityhealthcd.onmicrosoft.com>
2021-12-14 12:33:10 -0800

    Initial Commit

---
+++ test/SimpleAPI.TEST/UnitTest1.cs
@@ -0,0 +1,22 @@
+using Xunit;
+using SimpleAPI.Controllers;
+
+namespace SimpleAPI.TEST;
+
+public class UnitTest1
+{
+    WeatherForecastController controller  = new WeatherForecastController(null);
+
+    [Fact]
+    public void GetResults()
+    {
+        Assert.Equal(1, 1);
+    }
+
+
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
